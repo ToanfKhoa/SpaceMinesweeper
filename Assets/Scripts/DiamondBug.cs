b85bd9730223c878ab3cd4c7d9c27643171094c6
ToanfKhoa <quachbuitoankhoa@gmail.com>
2024-12-03 08:43:33 +0700

    AutoLaser code and animation

---
+++ Assets/Scripts/DiamondBug.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class DiamondBug : MonoBehaviour
+{
+    public float speed = 0.08f;
+    float randomDirection = 0;
+    public void Update()
+    {
+        if (randomDirection == 0)
+            randomDirection = Random.Range(0, 2) == 0 ? -1 : 1;
+        this.transform.position += new Vector3(randomDirection * speed, 0);
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+        for(int i = 0; i < 6; i++)
+            Game.Instance.InstantiateAndMove(Game.Instance.diamondPrefab, this.transform.position);
+        Game.Instance._userDatas.diamond += 5;
+        Game.Instance.SaveData();   
+    }
+}
